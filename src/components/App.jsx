123b8ac792f7ea6efb6054dd84c24effee305628
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-10-12 08:02:46 +0200

    working
    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Profile } from './Profile';
+import user from './Profile/user.json';
+// import { Statistics } from './Statistics';
+// import stat from './Statistics/statistics.json';
+// import { Friendlist } from './FriendList';
+// import friends from './FriendList/friends.json';
+// import { TransactionHistory } from './TransactionHistory';
+// import transaction from './TransactionHistory/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        // msFlexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
       }}
     >
-      React homework template
+      <Profile {...user}></Profile>
+      {/* <Statistics stats={stat}></Statistics>
+      <Friendlist friends={friends}></Friendlist>
+      <TransactionHistory item={transaction}></TransactionHistory> */}
     </div>
   );
 };
